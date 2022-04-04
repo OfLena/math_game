2ae16c55b139fd2c3fcf50dd8a75eef29578b357
Guy Dilena <guydilena@gmail.com>
2022-04-04 14:44:26 -0300

    Add: refactoring code to split between 4 files.

--- player.rb
+++ player.rb
@@ -1,33 +1,31 @@
 class Player
 
 attr_reader :name
-attr_accessor :lives
+attr_accessor :lives, :num1, :num2, :input
+
 
 
   def initialize(name)
     @name = name
     @lives = 3
+    @num1 = rand(0..10)
+    @num2 = rand(0..10)
   end
 
   def zero_lives?
     @lives == 0
   end
 
-  def ask_question
-    num1 = rand(0..10)
-    num2 = rand(0..10)
-    puts "#{@name} asks: What is #{num1} + #{num2} ?"
-  end
 
-  def answer_question
-    input = gets.chomp
-  end
 
-  def is_answer_correct?
-    if num1 + num2 = input
-      @lives -= 1
-    end
-  end
+  # def is_answer_correct?  
+  #   # puts @input
+  #   puts @num1
+  #   puts @num2
+  #   # @input = input
+  #   # if (@num1 + @num2) == @input ? @lives : @lives -= 1
+  #   # end
+  # end
 
 
 end
--- question.rb
+++ question.rb
@@ -1,22 +1,25 @@
-# require_relative './player'
+class Question
 
-# class Question
-
-#   attr_reader :name
-#   # attr_accessor :lives
-
-#   def initialize(name)
-#     @name = name
-#   end
-
-#   def ask_question
-#     num1 = rand(0..10)
-#     num2 = rand(0..10)
-#     puts "#{@name} asks: What is #{num1} + #{num2} ?"
-    
-#   end
-
-#   # def answer_question
-#   #   puts 
-#   # end
+  attr_reader :ask_question, :question_answer
+  
+  def initialize
+    num1 = rand(0..10)
+    num2 = rand(0..10)
+    @ask_question = "What is #{@num1} + #{@num2} ?"
+    @question_answer = num1 + num2
+  end
+end
+# def ask_question
+#   # @num1 = rand(0..10)
+#   # @num2 = rand(0..10)
+#   puts "#{@name} asks: What is #{@num1} + #{@num2} ?"
 # end
+
+# def answer_question
+#   input = gets.chomp
+#   # @input = input
+#   if @num1 + @num2 = input ? @lives : @lives -= 1
+#   end
+#   puts @num1
+# end
+
